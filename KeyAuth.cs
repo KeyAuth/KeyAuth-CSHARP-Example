db1efde0c5687db7c00624a6e4ce274d76cd646e
mak6969 <74461188+mak6969@users.noreply.github.com>
2020-11-28 17:06:54 -0500

    Update KeyAuth.cs

--- KeyAuth.cs
+++ KeyAuth.cs
@@ -62,8 +62,8 @@ namespace KeyAuth {
 	    var init_iv = encryption.sha256(encryption.iv_key()); // can be changed to whatever you want
             var values_to_upload = new NameValueCollection {
                 ["type"] = encryption.byte_arr_to_str(Encoding.Default.GetBytes("login")),
-                ["key"] = encryption.encrypt(key, secret, session_iv),
-                ["hwid"] = encryption.encrypt(hwid, secret, session_iv),
+                ["key"] = encryption.encrypt(key, secret, init_iv),
+                ["hwid"] = encryption.encrypt(hwid, secret, init_iv),
                 ["name"] = encryption.byte_arr_to_str(Encoding.Default.GetBytes(name)),
                 ["ownerid"] = encryption.byte_arr_to_str(Encoding.Default.GetBytes(ownerid)),
                 ["init_iv"] = init_iv
