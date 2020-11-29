80a31c9f46f41b7e52886aa20beb37f0884e865a
mak6969 <74461188+mak6969@users.noreply.github.com>
2020-11-29 10:03:03 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -12,9 +12,9 @@ namespace KeyAuth
     {
 
         static void Main(string[] args) {
-           string name = "Nigga"; // place your Application Name within the Quotes.
-           string ownerid = "DKjdsk232k22"; // place your OwnerID within the Quotes.
-           string secret = "301bbfe4ac5e874579e001b4b9dbd0ad04db5d359358a314cce58b3b24dd3bed"; // place your Application Secret within the Quotes.
+           string name = ""; // place your Application Name within the Quotes.
+           string ownerid = ""; // place your OwnerID within the Quotes.
+           string secret = ""; // place your Application Secret within the Quotes.
 
             
            api.init(name, secret, ownerid);
