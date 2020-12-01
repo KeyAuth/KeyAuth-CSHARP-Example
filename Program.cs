fa4ea4da6e40da37ba95bee3d327b6900246ecd5
mak6969 <74461188+mak6969@users.noreply.github.com>
2020-12-01 17:49:22 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using KeyAuth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = ""; // place your Application Name within the Quotes.
+            string ownerid = ""; // place your OwnerID within the Quotes.
+            string secret = ""; // place your Application Secret within the Quotes.
+
+            Console.Title = "KeyAuth C# Example";
+
+            api.init(name, secret, ownerid);
+
+            Console.WriteLine("\n\n  Enter key\n  ");
+            string key = Console.ReadLine();
+
+            api.login(key, name, secret, ownerid);
+
+            Console.WriteLine("\n\n  --Main Program--");
+            Console.ReadLine();
+        }
+    }
+}
