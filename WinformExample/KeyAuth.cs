ec62167079790c7bd1a97f9af8e4ad25a387f411
mak6969 <74461188+mak6969@users.noreply.github.com>
2021-01-26 00:23:42 -0500

    Update KeyAuth.cs

--- WinformExample/KeyAuth.cs
+++ WinformExample/KeyAuth.cs
@@ -11,10 +11,9 @@ using System.Runtime.Serialization;
 using System.Runtime.Serialization.Json;
 using System.Diagnostics;
 using System.Security.Principal;
-using System.Windows;
+using System.Threading;
 
-namespace KeyAuth
-{
+namespace KeyAuth {
     public class api
     {
         public string name, ownerid, secret, version;
@@ -23,7 +22,8 @@ namespace KeyAuth
 
             if (string.IsNullOrWhiteSpace(name) || string.IsNullOrWhiteSpace(ownerid) || string.IsNullOrWhiteSpace(secret) || string.IsNullOrWhiteSpace(version))
             {
-                MessageBox.Show("Application not setup correctly. Please watch video link found in Login.cs");
+                Console.WriteLine("\n\n  Application not setup correctly. Please watch video link found in Program.cs");
+                Thread.Sleep(3500);
                 Environment.Exit(0);
             }
 
@@ -87,7 +87,8 @@ namespace KeyAuth
 
             if (!json.success)
             {
-                MessageBox.Show(json.message);
+                Console.WriteLine("\n\n " + json.message);
+                Thread.Sleep(3500);
                 Environment.Exit(0);
             }
             else
@@ -97,9 +98,9 @@ namespace KeyAuth
 
         }
 
-        public bool login(string key, string hwid = null)
+        public void login(string key)
         {
-            if (hwid == null) hwid = WindowsIdentity.GetCurrent().User.Value;
+            string hwid = WindowsIdentity.GetCurrent().User.Value;
 
             var init_iv = encryption.sha256(encryption.iv_key()); // can be changed to whatever you want
 
@@ -120,16 +121,67 @@ namespace KeyAuth
 
             if (!json.success)
             {
-                MessageBox.Show(json.message);
-                return false;
+                Console.WriteLine("\n\n " + json.message);
+                Thread.Sleep(3500);
+                Environment.Exit(0);
             }
             else
             {
                 load_user_data(json.info);
-                return true;
+                File.WriteAllText(@"C:\ProgramData\" + name, key);
             }
         }
-        
+
+        public string var(string varid)
+        {
+            string hwid = WindowsIdentity.GetCurrent().User.Value;
+
+            var init_iv = encryption.sha256(encryption.iv_key()); // can be changed to whatever you want
+
+            var values_to_upload = new NameValueCollection
+            {
+                ["type"] = encryption.byte_arr_to_str(Encoding.Default.GetBytes("var")),
+                ["key"] = encryption.encrypt(user_data.key, secret, init_iv),
+                ["varid"] = encryption.encrypt(varid, secret, init_iv),
+                ["hwid"] = encryption.encrypt(hwid, secret, init_iv),
+                ["name"] = encryption.byte_arr_to_str(Encoding.Default.GetBytes(name)),
+                ["ownerid"] = encryption.byte_arr_to_str(Encoding.Default.GetBytes(ownerid)),
+                ["init_iv"] = init_iv
+            };
+
+            var response = req(values_to_upload);
+
+            response = encryption.decrypt(response, secret, init_iv);
+            var json = response_decoder.string_to_generic<response_structure>(response);
+
+            if (!json.success)
+            {
+                Console.WriteLine("\n\n " + json.message);
+                Thread.Sleep(3500);
+                return "";
+            }
+            else
+            {
+                return json.message;
+            }
+        }
+
+        public void download(string fileid, string path)
+        {
+            var init_iv = encryption.sha256(encryption.iv_key()); // can be changed to whatever you want
+
+            var values_to_upload = new NameValueCollection
+            {
+                ["type"] = encryption.byte_arr_to_str(Encoding.Default.GetBytes("file")),
+                ["fileid"] = encryption.encrypt(fileid, secret, init_iv),
+                ["name"] = encryption.byte_arr_to_str(Encoding.Default.GetBytes(name)),
+                ["ownerid"] = encryption.byte_arr_to_str(Encoding.Default.GetBytes(ownerid)),
+                ["init_iv"] = init_iv
+            };
+
+            var response = dl(values_to_upload, path);
+        }
+
         public void log(string message)
         {
             var init_iv = encryption.sha256(encryption.iv_key()); // can be changed to whatever you want
@@ -177,10 +229,39 @@ namespace KeyAuth
                     return Encoding.Default.GetString(raw_response);
                 }
             }
+            catch 
+            {
+                
+                Console.WriteLine("\n\n  SSL Pin Error. Please try again with apps that modify network activity closed/disabled.");
+                Thread.Sleep(3500);
+                Environment.Exit(0);
+                return "lmao";
+            }
+        }
+
+        private static string dl(NameValueCollection post_data, string path)
+        {
+            try
+            {
+                using (WebClient client = new WebClient())
+                {
+                    client.Headers["User-Agent"] = "KeyAuth";
+
+                    ServicePointManager.ServerCertificateValidationCallback = others.pin_public_key;
+
+                    byte[] result = client.UploadValues("https://keyauth.com/api/v2/", post_data);
+
+                    ServicePointManager.ServerCertificateValidationCallback += (send, certificate, chain, sslPolicyErrors) => { return true; };
+
+                    File.WriteAllBytes(path, result);
+                    return "";
+                }
+            }
             catch
             {
 
-                MessageBox.Show("SSL Pin Error. Please try again with apps that modify network activity closed/disabled.");
+                Console.WriteLine("\n\n  SSL Pin Error. Please try again with apps that modify network activity closed/disabled.");
+                Thread.Sleep(3500);
                 Environment.Exit(0);
                 return "lmao";
             }
@@ -190,14 +271,12 @@ namespace KeyAuth
         #region user_data
         public user_data_class user_data = new user_data_class();
 
-        public class user_data_class
-        {
+        public class user_data_class {
             public string key { get; set; }
             public DateTime expiry { get; set; }
             public int level { get; set; }
         }
-        private void load_user_data(user_data_structure data)
-        {
+        private void load_user_data(user_data_structure data) {
             user_data.key = data.key;
 
             user_data.expiry = others.unix_to_date(Convert.ToDouble(data.expiry));
@@ -209,8 +288,7 @@ namespace KeyAuth
         private json_wrapper response_decoder = new json_wrapper(new response_structure());
     }
 
-    public static class others
-    {
+    public static class others {
         public static DateTime unix_to_date(double unixTimeStamp) =>
     new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc).AddSeconds(unixTimeStamp).ToLocalTime();
 
@@ -218,18 +296,15 @@ namespace KeyAuth
             certificate.GetPublicKeyString() == "0480126A944139DFDCF7808EF35430F592F6C1BDDEF3AB693563B3521FFBBA907E0A44F99FF43B8A1D68CA89778AA06BEA97A72EFF4C1BBAB49F9B84F154D57944";
     }
 
-    public static class encryption
-    {
-        public static string byte_arr_to_str(byte[] ba)
-        {
+    public static class encryption {
+        public static string byte_arr_to_str(byte[] ba) {
             StringBuilder hex = new StringBuilder(ba.Length * 2);
             foreach (byte b in ba)
                 hex.AppendFormat("{0:x2}", b);
             return hex.ToString();
         }
 
-        public static byte[] str_to_byte_arr(string hex)
-        {
+        public static byte[] str_to_byte_arr(string hex) {
             int NumberChars = hex.Length;
             byte[] bytes = new byte[NumberChars / 2];
             for (int i = 0; i < NumberChars; i += 2)
@@ -237,20 +312,16 @@ namespace KeyAuth
             return bytes;
         }
 
-        public static string encrypt_string(string plain_text, byte[] key, byte[] iv)
-        {
+        public static string encrypt_string(string plain_text, byte[] key, byte[] iv) {
             Aes encryptor = Aes.Create();
 
             encryptor.Mode = CipherMode.CBC;
             encryptor.Key = key;
             encryptor.IV = iv;
 
-            using (MemoryStream mem_stream = new MemoryStream())
-            {
-                using (ICryptoTransform aes_encryptor = encryptor.CreateEncryptor())
-                {
-                    using (CryptoStream crypt_stream = new CryptoStream(mem_stream, aes_encryptor, CryptoStreamMode.Write))
-                    {
+            using (MemoryStream mem_stream = new MemoryStream()) {
+                using (ICryptoTransform aes_encryptor = encryptor.CreateEncryptor()) {
+                    using (CryptoStream crypt_stream = new CryptoStream(mem_stream, aes_encryptor, CryptoStreamMode.Write)) {
                         byte[] p_bytes = Encoding.Default.GetBytes(plain_text);
 
                         crypt_stream.Write(p_bytes, 0, p_bytes.Length);
@@ -265,20 +336,16 @@ namespace KeyAuth
             }
         }
 
-        public static string decrypt_string(string cipher_text, byte[] key, byte[] iv)
-        {
+        public static string decrypt_string(string cipher_text, byte[] key, byte[] iv) {
             Aes encryptor = Aes.Create();
 
             encryptor.Mode = CipherMode.CBC;
             encryptor.Key = key;
             encryptor.IV = iv;
 
-            using (MemoryStream mem_stream = new MemoryStream())
-            {
-                using (ICryptoTransform aes_decryptor = encryptor.CreateDecryptor())
-                {
-                    using (CryptoStream crypt_stream = new CryptoStream(mem_stream, aes_decryptor, CryptoStreamMode.Write))
-                    {
+            using (MemoryStream mem_stream = new MemoryStream()) {
+                using (ICryptoTransform aes_decryptor = encryptor.CreateDecryptor()) {
+                    using (CryptoStream crypt_stream = new CryptoStream(mem_stream, aes_decryptor, CryptoStreamMode.Write)) {
                         byte[] c_bytes = str_to_byte_arr(cipher_text);
 
                         crypt_stream.Write(c_bytes, 0, c_bytes.Length);
@@ -299,8 +366,7 @@ namespace KeyAuth
         public static string sha256(string r) =>
             byte_arr_to_str(new SHA256Managed().ComputeHash(Encoding.Default.GetBytes(r)));
 
-        public static string encrypt(string message, string enc_key, string iv)
-        {
+        public static string encrypt(string message, string enc_key, string iv) {
             byte[] _key = Encoding.Default.GetBytes(sha256(enc_key).Substring(0, 32));
 
             byte[] _iv = Encoding.Default.GetBytes(sha256(iv).Substring(0, 16));
@@ -308,8 +374,7 @@ namespace KeyAuth
             return encrypt_string(message, _key, _iv);
         }
 
-        public static string decrypt(string message, string enc_key, string iv)
-        {
+        public static string decrypt(string message, string enc_key, string iv) {
             byte[] _key = Encoding.Default.GetBytes(sha256(enc_key).Substring(0, 32));
 
             byte[] _iv = Encoding.Default.GetBytes(sha256(iv).Substring(0, 16));
@@ -324,13 +389,11 @@ namespace KeyAuth
             certificate.GetPublicKeyString() == "0480126A944139DFDCF7808EF35430F592F6C1BDDEF3AB693563B3521FFBBA907E0A44F99FF43B8A1D68CA89778AA06BEA97A72EFF4C1BBAB49F9B84F154D57944";
     }
 
-    public class json_wrapper
-    {
+    public class json_wrapper {
         public static bool is_serializable(Type to_check) =>
             to_check.IsSerializable || to_check.IsDefined(typeof(DataContractAttribute), true);
 
-        public json_wrapper(object obj_to_work_with)
-        {
+        public json_wrapper(object obj_to_work_with) {
             current_object = obj_to_work_with;
 
             var object_type = current_object.GetType();
@@ -341,10 +404,8 @@ namespace KeyAuth
                 throw new Exception($"the object {current_object} isn't a serializable");
         }
 
-        public string to_json_string()
-        {
-            using (var mem_stream = new MemoryStream())
-            {
+        public string to_json_string() {
+            using (var mem_stream = new MemoryStream()) {
                 serializer.WriteObject(mem_stream, current_object);
 
                 mem_stream.Position = 0;
@@ -354,8 +415,7 @@ namespace KeyAuth
             }
         }
 
-        public object string_to_object(string json)
-        {
+        public object string_to_object(string json) {
             var buffer = Encoding.Default.GetBytes(json);
 
             //SerializationException = session expired
@@ -365,7 +425,7 @@ namespace KeyAuth
         }
 
         #region extras
-
+        
         public dynamic string_to_dynamic(string json) =>
             (dynamic)string_to_object(json);
 
