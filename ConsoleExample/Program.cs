c82583f174e50f65ff54a833a6f968780a9aa913
William Nelson <83034852+wnelson03@users.noreply.github.com>
2021-06-23 10:21:46 -0400

    Update to 1.0 API endpoint

--- ConsoleExample/Program.cs
+++ ConsoleExample/Program.cs
@@ -1,19 +1,10 @@
 using System;
 using System.IO;
-using System.Linq;
 
 namespace KeyAuth
 {
     class Program
     {
-
-        private static Random random = new Random();
-        public static string RandomString(int length)
-        {
-            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-            return new string(Enumerable.Repeat(chars, length)
-              .Select(s => s[random.Next(s.Length)]).ToArray());
-        }
 
         /*
          * 
@@ -29,44 +20,17 @@ namespace KeyAuth
         public static api KeyAuthApp = new api(name, ownerid, secret, version);
 
         static void Main(string[] args) {
-            Console.Title = RandomString(10);
-            Console.ForegroundColor = ConsoleColor.Cyan;
+
+            Console.Title = "KeyAuth API Example";
             Console.WriteLine("\n\n  Connecting..");
             KeyAuthApp.init();
-
-
-            // Register and Login Functions (still in beta)
-            // KeyAuthApp.register("username", "password", "key");
-            //KeyAuthApp.login("username", "password"); 
-
-            string key;
-            if (!File.Exists(@"C:\ProgramData\" + name))
-            {
-                Console.WriteLine("\n\n  Please enter your license key:\n\n  ");
-                key = Console.ReadLine();
-                KeyAuthApp.license(key);
-            }
-            else
-            {
-                key = File.ReadAllText(@"C:\ProgramData\" + name);
-                Console.WriteLine("\n\n  Logging in with saved key: " + key);
-                KeyAuthApp.license(key);
-            }
+            Console.Write("\n\n Enter your license key: ");
+            string key = Console.ReadLine();
+            KeyAuthApp.license(key);
             Console.WriteLine("\n\n  Logged In!");
+            // byte[] result = KeyAuthApp.download("902901"); // downloads application file
+            // File.WriteAllBytes("C:\\Users\\mak\\Downloads\\KeyAuth-CSHARP-Example-main (5)\\KeyAuth-CSHARP-Example-main\\ConsoleExample\\bin\\Debug\\countkeys.txt", result);
             Console.ReadLine();
-            Console.WriteLine("\n\n  Your key expires at " + KeyAuthApp.user_data.expiry);
-
-            Console.ReadLine(); // remove this. this is for testing purposes. it's whats keeping your application open forever
-            /*
-            Optional Functions:
-
-            KeyAuthApp.webhook("lfvbBrbFhI", "?sellerkey=CUqDqlCIgl&type=resethash");
-            // send secure request to webhook which is impossible to crack into. the base link set on the website is https://keyauth.com/api/seller/, which nobody except you can see, so the final request is https://keyauth.com/api/seller/?sellerkey=CUqDqlCIgl&type=resethash
-
-            KeyAuthApp.download("231696", "C:\\ok.dll"); // downloads application file
-
-            Console.WriteLine(KeyAuthApp.var("123456")); // retrieves application variable
-            */
         }
     }
 }
