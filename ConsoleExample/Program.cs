8b4309388748fe69a53fd64a7b15ce24c7146594
William Nelson <83034852+wnelson03@users.noreply.github.com>
2021-07-28 17:36:14 -0400

    Remove space from secret string

--- ConsoleExample/Program.cs
+++ ConsoleExample/Program.cs
@@ -18,7 +18,7 @@ namespace KeyAuth
 
         static string name = ""; // application name. right above the blurred text aka the secret on the licenses tab among other tabs
         static string ownerid = ""; // ownerid, found in account settings. click your profile picture on top right of dashboard and then account settings.
-        static string secret = "    "; // app secret, the blurred text on licenses tab and other tabs
+        static string secret = ""; // app secret, the blurred text on licenses tab and other tabs
         static string version = "1.0"; // leave alone unless you've changed version on website
 
         public static api KeyAuthApp = new api(name, ownerid, secret, version);
